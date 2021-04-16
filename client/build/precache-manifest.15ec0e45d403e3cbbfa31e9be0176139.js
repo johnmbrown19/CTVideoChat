3a89951b9719bd83a7e931e9f7948071549a5762
John McLeod Brown <johnmcleodbrown@gmail.com>
2021-04-16 20:42:16 +0100

    updated gitignore

---
+++ client/build/precache-manifest.15ec0e45d403e3cbbfa31e9be0176139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f538d77f5a0e53881a29c68de9947857",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2b30a094b1051d79ab7",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "1fdd29bb48e99ec91be8",
+    "url": "/static/js/2.68a5f081.chunk.js"
+  },
+  {
+    "revision": "f89ec0e947fe52779183e6ebdf1dc0ff",
+    "url": "/static/js/2.68a5f081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b30a094b1051d79ab7",
+    "url": "/static/js/main.dd067443.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
